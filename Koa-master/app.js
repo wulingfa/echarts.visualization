ae8e173836a79745a6921793e24e25655f7f8e4f
jackwu <2797867183@qq.com>
2021-10-09 15:15:32 +0800

    one

---
+++ Koa-master/app.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+//导入响应耗时中间件
+const resDurationMiddleware = require('./middleware/koa_response_duration');
+//响应头中间件
+const resHeaderMiddleware = require('./middleware/koa_response_header')
+//处理业务逻辑的中间件
+const resDataMiddleware = require('./middleware/koa_response_data')
+const app = new Koa();
+app.use(resDurationMiddleware);
+app.use(resHeaderMiddleware);
+app.use(resDataMiddleware)
+app.listen(3000)
+
+const WebSocketService = require('./service/ws_service')
+WebSocketService.listen()
